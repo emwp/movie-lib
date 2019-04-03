5e11df22279486b86cce6c0336be218cf11462f3
Everton Pereira <everton.mwp@gmail.com>
2019-04-03 20:37:35 -0300

    Add react-slick and slick-carousel dependencies

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
